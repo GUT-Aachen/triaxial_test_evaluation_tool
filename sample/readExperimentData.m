8fe2cc76e27233b1a8ca454a2430d599ca2c2233
froido <froido@gmx.de>
2019-10-22 17:37:37 +0200

    * Match camelCase

--- sample/readExperimentData.m
+++ sample/readExperimentData.m
@@ -11,44 +11,44 @@ close all
 
 
 %Initialize database
-db = MeridDB('hiwi_ro','#meridDB2019','134.130.87.47',3306);
+dbConnection = MeridDB('hiwi_ro','#meridDB2019','134.130.87.47',3306);
 
 %Request all experiments and print out
-experimentOverview = db.getExperiments;
+experimentOverview = dbConnection.getExperiments;
 fprintf('\nFound the following experiments in database:\n')
 disp(experimentOverview);
 
-experiment_no = 0;
+experimentNo = 0;
 
 %Select your experiment as long as the experiment is not valid, the input
 %dialog will be repeated.
-while sum(ismember(experimentOverview.experiment_no,experiment_no)) < 1
-    experiment_no = inputdlg('Select an experiment number: ','ExperimentNo',1);
+while sum(ismember(experimentOverview.experimentNo,experimentNo)) < 1
+    experimentNo = inputdlg('Select an experiment number: ','ExperimentNo',1);
     
     %Error if cancel
-    if (isempty(experiment_no))
+    if (isempty(experimentNo))
         error('Empty message or input dialog canceled')
     end
-    experiment_no = str2num(experiment_no{1});
+    experimentNo = str2num(experimentNo{1});
 end
 
 %Get metadata from database
 fprintf('Get data from database \n');
-db_metadata = db.getMetaData(experiment_no);
+dbMetaData = dbConnection.getMetaData(experimentNo);
 
 %Check if metadata was submitted
-if (db_metadata == 0)
+if (dbMetaData == 0)
     %stop execution when no metadata was found
     return;
 end
 
 %Get specimen and rock data from database
-db_specimendata = db.getSpecimenData(experiment_no, db_metadata.specimenId);
+dbSpecimenData = dbConnection.getSpecimenData(experimentNo, dbMetaData.specimenId);
 
 %Get experiments data
-db_data = db.getExperimentData(experiment_no);
+dbData = dbConnection.getExperimentData(experimentNo);
 
 %%
-permeabilityTable=db_data.getPermeability(db_specimendata.height,db_specimendata.diameter,0.5);
+permeabilityTable=dbData.getPermeability(dbSpecimenData.height,dbSpecimenData.diameter,0.5);
 
 %%
