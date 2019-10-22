6c9341f04846df97c0cd0657c47c483892e496c0
froido <froido@gmx.de>
2019-10-22 17:46:32 +0200

    Match camelCase

--- classes/ExperimentsMetaData.m
+++ classes/ExperimentsMetaData.m
@@ -22,8 +22,8 @@ classdef ExperimentsMetaData < handle
     properties (SetAccess = private)
         description; %Proxyvariable for description in metadata. Data is only saved in metaDataTable.
         comment; %Proxyvariable for comments in metadata. Data is only saved in metaDataTable.
-        time_start; %Proxyvariable for start time in metadata. Data is only saved in metaDataTable.
-        time_end; %Proxyvariable for end time in metadata. Data is only saved in metaDataTable.
+        timeStart; %Proxyvariable for start time in metadata. Data is only saved in metaDataTable.
+        timeEnd; %Proxyvariable for end time in metadata. Data is only saved in metaDataTable.
         short; %Proxyvariable for short name in metadata. Data is only saved in metaDataTable.
         specimenId; %Proxyvariable for specimen id in metadata. Data is only saved in metaDataTable.
         fluidPressure; %Proxyvariable for fluid pressure used in metadata. Data is only saved in metaDataTable.
@@ -44,39 +44,45 @@ classdef ExperimentsMetaData < handle
         function obj = setMetaDataAsTable(obj, metaDataTable)
         %Set all metadata for the experiment into the object.
         %Input parameter: table with metaData
-            
-            %Check if metadata fits to set experiment number
-            if (metaDataTable.experiment_no(1) == obj.experimentNo)
-                obj.metaDataTable = metaDataTable;
-                
-                obj.metaDataTable.Properties.VariableUnits{'pressure_fluid'} = 'kN/m²';
-                obj.metaDataTable.Properties.VariableUnits{'pressure_confining'} = 'kN/m²';
-                
-                %Recasting datetime-String to real Datetime in Matlab
-                obj.metaDataTable.time_start = datetime(obj.metaDataTable.time_start,'InputFormat','yyyy-MM-dd HH:mm:ss.S');
-                obj.metaDataTable.time_end = datetime(obj.metaDataTable.time_end,'InputFormat','yyyy-MM-dd HH:mm:ss.S');
-                
-                disp([class(obj), ': ', 'Metadata set sucessfully']);
-            else
-               error('Given metadata do not fit to set experiment number');
+            try
+                %Check if metadata fits to set experiment number
+                if (metaDataTable.experimentNo(1) == obj.experimentNo)
+                    obj.metaDataTable = metaDataTable;
+
+                    obj.metaDataTable.Properties.VariableUnits{'pressureFluid'} = 'kN/m²';
+                    obj.metaDataTable.Properties.VariableUnits{'pressureConfining'} = 'kN/m²';
+
+                    %Recasting datetime-String to real Datetime in Matlab
+                    obj.metaDataTable.timeStart = datetime(obj.metaDataTable.timeStart,'InputFormat','yyyy-MM-dd HH:mm:ss.S');
+                    obj.metaDataTable.timeEnd = datetime(obj.metaDataTable.timeEnd,'InputFormat','yyyy-MM-dd HH:mm:ss.S');
+
+                    disp([class(obj), ': ', 'Metadata set sucessfully']);
+                else
+                   error('Given metadata do not fit to set experiment number');
+                end
+            catch E
+                warning([class(obj), ': ', 'Error while setting meta data']);
             end
         end
         
         function obj = setTimeLogAsTable(obj, timeLogTable)
         %Set time log for the experiment into the object.
         %Input parameter: table with time log
-            
-            %Check if timelog fits to set experiment number
-            if (timeLogTable.experiment_no(1) == obj.experimentNo)
-                obj.timeLog = timeLogTable;
-                
-                %Recasting datetime-String to real Datetime in Matlab
-                obj.timeLog.time = datetime(obj.timeLog.time,'InputFormat','yyyy-MM-dd HH:mm:ss.S');
-                
-                
-                disp([class(obj), ': ', 'Time log set sucessfully']);
-            else
-               error('Given time log do not fit to set experiment number');
+            try
+                %Check if timelog fits to set experiment number
+                if (timeLogTable.experiment_no(1) == obj.experimentNo)
+                    obj.timeLog = timeLogTable;
+
+                    %Recasting datetime-String to real Datetime in Matlab
+                    obj.timeLog.time = datetime(obj.timeLog.time,'InputFormat','yyyy-MM-dd HH:mm:ss.S');
+
+
+                    disp([class(obj), ': ', 'Time log set sucessfully']);
+                else
+                   error('Given time log do not fit to set experiment number');
+                end
+            catch E
+                warning([class(obj), ': ', 'Error while setting time log']);
             end
         end
         
@@ -96,20 +102,20 @@ classdef ExperimentsMetaData < handle
             obj.metaDataTable.comment(1) = cellstr(comment);
         end
         
-        function obj = setTime_start(obj,time_start) 
+        function obj = setTime_start(obj,timeStart) 
         %Set a start time for the experiment into the metadata. The
-        %information will not be saved into the variable 'time_start'. It
+        %information will not be saved into the variable 'timeStart'. It
         %is saved into the variable metaDataTable instead.
         %Input parameter: start time as char
-            obj.metaDataTable.time_start(1) = cellstr(time_start);
+            obj.metaDataTable.timeStart(1) = cellstr(timeStart);
         end
         
-        function obj = setTime_end(obj,time_end)
+        function obj = setTime_end(obj,timeEnd)
         %Set a end time for the experiment into the metadata. The
-        %information will not be saved into the variable 'time_end'. It
+        %information will not be saved into the variable 'timeEnd'. It
         %is saved into the variable metaDataTable instead.
         %Input parameter: end time as char
-            obj.metaDataTable.time_end(1) = cellstr(time_end);
+            obj.metaDataTable.timeEnd(1) = cellstr(timeEnd);
         end
         
         function obj = setShort(obj,short) 
@@ -133,28 +139,28 @@ classdef ExperimentsMetaData < handle
             comment = obj.metaDataTable.comment{1};
         end
         
-        function time_start = get.time_start(obj) 
-            time_start = obj.metaDataTable.time_start(1);
+        function timeStart = get.timeStart(obj) 
+            timeStart = obj.metaDataTable.timeStart(1);
         end
         
-        function time_end = get.time_end(obj) 
-            time_end = obj.metaDataTable.time_end(1);
+        function timeEnd = get.timeEnd(obj) 
+            timeEnd = obj.metaDataTable.timeEnd(1);
         end
         
         function short = get.short(obj) 
             short = obj.metaDataTable.short{1};
         end
         
-        function specimen_id = get.specimenId(obj) 
-            specimen_id = obj.metaDataTable.specimen_id;
+        function specimenId = get.specimenId(obj) 
+            specimenId = obj.metaDataTable.specimenId;
         end
         
-        function pressure_fluid = get.fluidPressure(obj) 
-            pressure_fluid = obj.metaDataTable.pressure_fluid;
+        function pressureFluid = get.fluidPressure(obj) 
+            pressureFluid = obj.metaDataTable.pressureFluid;
         end
         
-        function pressure_confining = get.confiningPressure(obj) 
-            pressure_confining = obj.metaDataTable.pressure_confining;
+        function pressureConfining = get.confiningPressure(obj) 
+            pressureConfining = obj.metaDataTable.pressureConfining;
         end
         
         function timeLog = get.timeLog(obj) 
