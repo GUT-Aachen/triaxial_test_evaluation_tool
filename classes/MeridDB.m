68fc03360f5ea71b32b4f9d7937bf6ffe01164e3
froido <froido@gmx.de>
2020-11-12 20:55:06 +0100

    *catchFromDatabase() correct timeStart and timeEnd in MySQL query

--- classes/MeridDB.m
+++ classes/MeridDB.m
@@ -188,11 +188,10 @@ classdef MeridDB < handle
             %Extract the data, taking into account the maximum number of rows
             for i = 0:steps-1
 				if ~isempty(timeStart) && ~isempty(timeEnd)
-					% dbQuery = char(strcat('SELECT * FROM `', tableName, '` WHERE `experiment_no`= ',int2str(experimentNo), ' AND `time` BETWEEN ''', timeStart, ''' AND ''', timeEnd, ''' LIMIT ',{' '}, int2str(i*selectionLimit) ,',', int2str(selectionLimit)));
-					
+					dbQuery = char(strcat("SELECT * FROM `", tableName, "` WHERE `experiment_no`= ",int2str(experimentNo), " AND `time` BETWEEN STR_TO_DATE('", timeStart, "', '%Y/%m/%d %H:%i:%s') AND STR_TO_DATE('", timeEnd, "', '%Y/%m/%d %H:%i:%s') LIMIT ", int2str(i*selectionLimit) ,",", int2str(selectionLimit)));
 					% convert timeStart and timeEnd from UTC to MySQL system time to compare successfull to datetime values
 					% in table
-					dbQuery = char(strcat("SELECT * FROM `", tableName, "` WHERE `experiment_no`= ",int2str(experimentNo), " AND `time` BETWEEN CONVERT_TZ(STR_TO_DATE('", timeStart, "', '%Y/%m/%d %H:%i:%s'), '+00:00', 'system') AND CONVERT_TZ(STR_TO_DATE('", timeEnd, "', '%Y/%m/%d %H:%i:%s'), '+00:00', 'system') LIMIT ", int2str(i*selectionLimit) ,",", int2str(selectionLimit)));
+					%dbQuery = char(strcat("SELECT * FROM `", tableName, "` WHERE `experiment_no`= ",int2str(experimentNo), " AND `time` BETWEEN CONVERT_TZ(STR_TO_DATE('", timeStart, "', '%Y/%m/%d %H:%i:%s'), '+00:00', 'system') AND CONVERT_TZ(STR_TO_DATE('", timeEnd, "', '%Y/%m/%d %H:%i:%s'), '+00:00', 'system') LIMIT ", int2str(i*selectionLimit) ,",", int2str(selectionLimit)));
 					dbResult = select(dbConnection,dbQuery);
 				else
 					dbQuery = char(strcat('SELECT * FROM `', tableName, '` WHERE `experiment_no`= ',int2str(experimentNo), ' LIMIT ',{' '}, int2str(i*selectionLimit) ,',', int2str(selectionLimit)));
@@ -342,6 +341,7 @@ classdef MeridDB < handle
                         warning('No time log found for experiment!')
                     else
                         dbResult.Properties.VariableNames = {'experimentNo' 'retrospective' 'time' 'description'}; %renaming columns in result table to match camelCase
+						dbResult.time = datetime(dbResult.time, 'TimeZone', 'UTC');
                         metaData.setTimeLogAsTable(dbResult);
                     end
                     
